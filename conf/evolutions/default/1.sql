59be8bc4e77258730631657dbe6c381e54750baf
Philip Johnson <johnson@hawaii.edu>
2013-07-23 09:19:10 -1000

    Adding the db evolution script back in.

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,22 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table page_retrieval (
+  primary_key               bigint auto_increment not null,
+  timestamp                 bigint,
+  constraint pk_page_retrieval primary key (primary_key))
+;
+
+
+
+
+# --- !Downs
+
+SET FOREIGN_KEY_CHECKS=0;
+
+drop table page_retrieval;
+
+SET FOREIGN_KEY_CHECKS=1;
+
